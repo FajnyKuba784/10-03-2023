5c45f64c91bdfce0ee652645bc1ce018921c2324
FajnyKuba784 <paruwka150@gmail.com>
2023-03-16 13:07:39 +0100

    ez

--- admij.js
+++ admij.js
@@ -18,6 +18,7 @@ async function getDatat(){
         const h3 = document.createElement("h3")
         const p = document.createElement("p")
         
+        div.setAttribute("class","diva")
         lista.appendChild(div)
         h3.innerHTML = ("Głos: "+json[i].kandydat)
         div.appendChild(h3)
@@ -28,32 +29,48 @@ async function getDatat(){
         if(json[i].kandydat=="Obama2") k2++
         if(json[i].kandydat=="Obama3") k3++
     }
-
+    
     var xValues = ["Obama1","Obama2","Obama3"];
     var yValues = [k1,k2,k3];
     var barColors = ["#3D2645","#832161","#991731"];
     
-
+    
     var myChart = new Chart("myChart",{
-    type: "pie",
-    data: {
-        labels: xValues,
-    datasets: [{
-    backgroundColor: barColors,
-    data: yValues
-    }]
-    },
-    options: {
-    title: {
-    display: true,
-    text: "Liczba Głosów"
-}
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yValues
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Liczba Głosów"
+            }
 }
 })
 
 
-    
-    
+
+
+
+lider()
 }
 getDatat()
 
+function lider(){
+if(k1>k2&&k1>k3){
+
+    document.getElementById("lider").innerHTML = "Aktualny lider: Obama1"
+}
+if(k2>k1&&k2>k3){
+    
+    document.getElementById("lider").innerHTML = "Aktualny lider: Obama2"
+}
+if(k3>k1&&k3>k2){
+    
+    document.getElementById("lider").innerHTML = "Aktualny lider: Obama3"
+} 
+}
